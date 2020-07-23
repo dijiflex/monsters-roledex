3f3291d4ac83a0d55b054657dc5dfb7097dce282
dijiflex <dijiflex@gmail.com>
2020-07-23 22:04:37 +0300

    Updates

---
+++ precache-manifest.7ceed8bf9ddcf3e3a9a229ce4209cee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c5df603799adb7961a16e2316666ad",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "f7a4ac794984688eaa3c",
+    "url": "/monsters-roledex/static/css/main.7f4bbcc1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-roledex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a4ac794984688eaa3c",
+    "url": "/monsters-roledex/static/js/main.c43bffcc.chunk.js"
+  },
+  {
+    "revision": "6d5b5ec4dac54096337c",
+    "url": "/monsters-roledex/static/js/runtime-main.f85c0cb9.js"
+  }
+]);
